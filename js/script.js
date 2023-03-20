1332267ad3b83adb81eb29cb730af49b9278c45b
davvvead <davead461@gmail.com>
2023-03-20 23:42:39 +0000

    responsiveness

--- js/script.js
+++ js/script.js
@@ -3,13 +3,8 @@ const alertBtn = document.getElementById('alert-btn');
 const btt = document.getElementsByClassName('btt');
 const alertBox =  document.getElementById('alert');
 alertBtn.addEventListener('click', alertTrigger);
-btt.addEventListener('click', bttTrigger);
 
 function alertTrigger(){
     alertBox.classList.remove('hide');
-}
-
-function bttTrigger(){
-    btt.preventDefault();
 }
 
