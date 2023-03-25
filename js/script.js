8f5a8df78c74193588c67a97eb9b5b05a682be5a
davvvead <davead461@gmail.com>
2023-03-25 22:55:13 +0000

    Mobile Nav JS

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ function alertTrigger(){
     alertBox.classList.remove('hide');
 }
 
+// Mobile Toggle
+const mobile = document.querySelector('.fa-bars');
+const closeMobileNav = document.querySelector('.close');
+const mobileNav = document.querySelector('.mobile-nav');
+
+// Listening for a click on the mobile toggle
+mobile.onclick = function toggleMoblie(){
+    if(mobileNav.style.display = 'none'){
+        mobileNav.style.display = 'block';
+    }
+}
+
+// Listening for a click on the mobile nav close button
+closeMobileNav.onclick = function closeMoblie(){
+    if(mobileNav.style.display != 'none'){
+        mobileNav.style.display = 'none';
+    }
+}
+
